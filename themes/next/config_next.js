23b63a73371d6ac39af63904b6b7cb0ab6f282c4
韭皇啦 <51317376+zyancn@users.noreply.github.com>
2022-12-22 18:58:56 +0800

    Update config_next.js

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -1,9 +1,9 @@
 const CONFIG_NEXT = {
 
-  HOME_BANNER: false, // 首页是否显示大图及标语 [true,false]
+  HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
   HOME_BANNER_Strings: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
-  NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部、屏幕顶部自动折叠，不固定
+  NAV_TYPE: 'autoCollapse', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部、屏幕顶部自动折叠，不固定
 
   POST_LIST_COVER: false, // 文章列表显示封面图
   POST_LIST_PREVIEW: true, // 显示文章预览
